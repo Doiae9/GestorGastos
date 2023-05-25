3e7c25c715d0ab5cd7b7e732540b285ee61080ab
Doiae9 <leonardogh75@gmail.com>
2023-05-25 01:25:12 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace GestorGastos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.botonAgregar = new System.Windows.Forms.Button();
+            this.Eliminar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ButtonInformes = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // botonAgregar
+            // 
+            this.botonAgregar.Location = new System.Drawing.Point(12, 12);
+            this.botonAgregar.Name = "botonAgregar";
+            this.botonAgregar.Size = new System.Drawing.Size(75, 23);
+            this.botonAgregar.TabIndex = 0;
+            this.botonAgregar.Text = "agregar";
+            this.botonAgregar.UseVisualStyleBackColor = true;
+            this.botonAgregar.Click += new System.EventHandler(this.botonAgregar_Click);
+            // 
+            // Eliminar
+            // 
+            this.Eliminar.Location = new System.Drawing.Point(12, 67);
+            this.Eliminar.Name = "Eliminar";
+            this.Eliminar.Size = new System.Drawing.Size(75, 23);
+            this.Eliminar.TabIndex = 1;
+            this.Eliminar.Text = "Eliminar";
+            this.Eliminar.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ButtonInformes
+            // 
+            this.ButtonInformes.Location = new System.Drawing.Point(192, 11);
+            this.ButtonInformes.Name = "ButtonInformes";
+            this.ButtonInformes.Size = new System.Drawing.Size(75, 23);
+            this.ButtonInformes.TabIndex = 3;
+            this.ButtonInformes.Text = "Informes";
+            this.ButtonInformes.UseVisualStyleBackColor = true;
+            this.ButtonInformes.Click += new System.EventHandler(this.ButtonInformes_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ButtonInformes);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Eliminar);
+            this.Controls.Add(this.botonAgregar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button botonAgregar;
+        private System.Windows.Forms.Button Eliminar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button ButtonInformes;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClaseGasto;
+
+
+
+namespace GestorGastos
+{
+    public partial class Form1 : Form
+    {
+        double presupuesto;
+        ListView listViewGastos;
+        List<Gasto> ListaGastos = new List<Gasto>();
+
+        public Form1()
+        {
+            Gasto si = new Gasto();
+
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void LimpiarCampos()
+        {
+            cmbCategoria.SelectedIndex = -1; // Deseleccionar cualquier categoría seleccionada
+            txtMonto.Text = string.Empty; // Vaciar el campo de monto
+            dtpFecha.Value = DateTime.Today; // Establecer la fecha actual
+            lblId.Text = string.Empty; // Vaciar la etiqueta de ID
+        }
+        private int GetNextId()
+        {
+            int maxId = 0;
+
+            // Obtener el máximo ID actual en la lista de gastos
+            foreach (Gasto gasto in gastos)
+            {
+                if (gasto.Id > maxId)
+                {
+                    maxId = gasto.Id;
+                }
+            }
+
+            // Incrementar el máximo ID para obtener el siguiente ID disponible
+            int nextId = maxId + 1;
+
+            return nextId;
+        }
+        private void botonAgregar_Click(object sender, EventArgs e)
+        {
+            Gasto NuevoGasto = new Gasto();
+            //ComboBox Agregar Categoria
+            NuevoGasto.Categoria = cmbCategoria.Text;
+            //Agregar Fecha con un CateTimePicker
+            NuevoGasto.Fecha = dtpFecha.Value;
+            //textbox Agregar Descripcion
+            NuevoGasto.Descripcion = txtDescripcion.Text.TrimEnd();
+            //textbox monto
+            NuevoGasto.Monto = Convert.ToDouble(txtMonto.Text);
+            NuevoGasto.ID = GetNextId();
+            presupuesto -= NuevoGasto.Monto;
+            if (presupuesto >= 0) {
+
+                ListaGastos.Add(NuevoGasto);
+                LimpiarCampos();
+            }
+            else
+            {
+                //Tus gastos exeden tu presupuesto.
+            }
+
+
+
+            //Espacio para agregar el texto 
+
+            //Cada vez que se cree un objeto se va a estar restando del presupuesto inicial. 
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int UserId = txtMonto.Text.TrimEnd();
+
+            //Boton eliminar
+            //A continuación se va a desplegar todos los datos que se encuentran almacenados, para que de esta forma,se generen los datos que se desean cambiar
+            //Se va a utilizar una lisview para mostrar los datos
+            //Iterador será la variable del mismo tipo que el opbjeto, el cual nos va a servir para igualar los datos 
+            if (listViewGastos.SelectedItems.Count > 0)
+            {
+                // Obtener el elemento seleccionado y eliminarlo del ListView
+                ListViewItem selectedItem = listViewGastos.SelectedItems[0];
+                listViewGastos.Items.Remove(selectedItem);
+
+                // Limpiar los controles de edición
+                LimpiarCampos();
+            }
+        }
+    }
+        }
+
+        private void ButtonInformes_Click(object sender, EventArgs e)
+        {
+            DateTime fechaFiltro = dtpFechaFiltro.value;
+            //Este no debería ser un arreglo.
+            string[] categoriaFiltro = cmbCategoriaFiltro.Text;
+
+            List<Gasto> GastosFiltrados = new List<Gasto>;
+
+            foreach (Gasto iterador in ListaGastos)
+
+            {
+                //Revisa categoria, no comprendo porque tengo error en iterador
+                if (iterador.Categoria = categoriaFiltro.[iterador.Categoria])
+                {
+                    GastosFiltrados.Add(iterador);
+                }
+                if (iterador.Fecha.Month == fechaFiltro.Month)
+                {
+                    GastosFiltrados.Add(iterador);
+                }
+                if (!string.IsNullOrEmpty(categoriaFiltro))
+                {
+                    //Solo funciona sin el array
+                    GastosFiltrados = GastosFiltrados.Where(g => g.Categoria == categoriaFiltro).ToList();
+                }
+                listViewGastos.Items.Clear();
+
+    // Agregar los gastos filtrados al ListView
+    foreach (Gasto gasto in GastosFiltrados)
+    {
+        string[] row = {
+            gasto.Categoria,
+            gasto.Monto.ToString(),
+            gasto.Fecha.ToString(),
+            gasto.Id.ToString()
+        };
+
+        ListViewItem listViewItem = new ListViewItem(row);
+        listViewGastos.Items.Add(listViewItem);
+    }
+}
+            }
+
+        }
+
+
+
+    }
+}
+
+
